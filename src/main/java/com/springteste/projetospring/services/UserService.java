511ca8869b553cadb294eed9c4f820338f3571aa
Pedro Henrique Fernandes Santos <pedro.h.santos@basis.com.br>
2020-11-10 12:16:11 -0300

    Dev profile + Postgres

--- src/main/java/com/springteste/projetospring/services/UserService.java
+++ src/main/java/com/springteste/projetospring/services/UserService.java
@@ -3,6 +3,8 @@ package com.springteste.projetospring.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -13,41 +15,43 @@ import com.springteste.projetospring.repositories.UserRepository;
 import com.springteste.projetospring.services.exceptions.DatabaseException;
 import com.springteste.projetospring.services.exceptions.ResourceNotFoundException;
 
-
 @Service
 public class UserService {
-	
-	
+
 	@Autowired
 	UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		try {
 			repository.deleteById(id);
-		}catch(EmptyResultDataAccessException e) {
+		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
-		}catch(DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
 	}
-	
+
 	public User update(Long id, User obj) {
-		User entity = repository.getOne(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
